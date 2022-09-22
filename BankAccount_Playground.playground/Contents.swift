b9c616a52056008ab485f0a7f20837355ffe0f6d
Sean Milfort <seanmilfort@seanzie-macmini.arrowcave>
2022-09-22 10:13:29 -0400

    Finished Bank Account Playground

---
+++ BankAccount_Playground.playground/Contents.swift
@@ -0,0 +1,90 @@
+func getBalanceInfo() -> String {
+    "Current balance: $\(balance)"
+}
+func withdraw(_ amount: Int) {
+    balance -= amount
+    print("Withdrew: $\(amount). \(getBalanceInfo())")
+}
+func debitWithdraw(_ amount: Int) {
+    if amount > balance {
+        print("Insufficient funds to withdraw $\(amount). \(getBalanceInfo())")
+    } else {
+        withdraw(amount)
+    }
+}
+func deposit(_ amount: Int) {
+    balance += amount
+    print("Deposited $\(amount). \(getBalanceInfo())")
+}
+func creditDeposit(_ amount: Int) {
+    deposit(amount)
+    if balance == 0 {
+        print("Paid off account balance.")
+    } else if balance > 0 {
+        print("Overpaid account balance.")
+    }
+}
+print("Welcome to your virtual bank system. What kind of account would you like to make?")
+print("1. Debit account")
+print("2. Credit account")
+print("3. Bank account")
+var accountType = ""
+var input = 0
+repeat {
+    print("Which option do you choose? (1, 2 or 3)")
+    input = Int.random(in: 1...3)
+    print("Selected option: \(input)")
+    switch input {
+    case 1: accountType = "debit"
+    case 2: accountType = "credit"
+    case 3: accountType = "generic"
+    default: break
+    }
+} while accountType == ""
+var balance = 100
+let transferAmount = 50
+print("Current balance: $\(balance)")
+print("Transfer amount: $\(transferAmount)")
+
+var transferType = ""
+
+func transfer(_ transferType: String) {
+    switch transferType {
+    case "withdraw": if accountType == "debit" {
+        debitWithdraw(_: transferAmount)
+    }else {
+        withdraw(_: transferAmount)
+    }
+    case "deposit": if accountType == "credit" {
+        creditDeposit(_: transferAmount)
+    } else {
+        deposit(_: transferAmount)
+    }
+    default: break
+    }
+}
+
+var isSystemOpened = true
+var option = 0
+
+repeat {
+    print("What would you like to do?")
+    print("1. Check bank account")
+    print("2. Withdraw money")
+    print("3. Deposit money")
+    print("4. Close the system")
+    print("Which option do you chose? (1, 2, 3, 4")
+    option = Int.random(in: 1...5)
+    print("The selected option is \(option).")
+    
+    switch option {
+    case 1: getBalanceInfo()
+    case 2: transfer("withdraw")
+    case 3: transfer("deposit")
+    case 4: isSystemOpened = false
+        print("The system is closed")
+    default: break
+    }
+    
+} while (isSystemOpened)
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# BankingAccountPlayground
